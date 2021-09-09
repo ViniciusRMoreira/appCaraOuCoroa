4870f4f3d8ed2ba7fb544d2d0cd7f07683030a38
Vinicius Rodolfo <viniciusrodolfomoreira@hotmail.com>
2021-09-09 12:45:54 -0300

    app cara ou coroa

---
+++ lib/resultado.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+class Resultado extends StatefulWidget {
+  String valor;
+  Resultado(this.valor);
+  @override
+  _ResultadoState createState() => _ResultadoState();
+}
+
+class _ResultadoState extends State<Resultado> {
+  @override
+  Widget build(BuildContext context) {
+    var caminhoimagem;
+    if (widget.valor == "cara") {
+      caminhoimagem = "images/moeda_cara.png";
+    } else {
+      caminhoimagem = "images/moeda_coroa.png";
+    }
+
+    return Scaffold(
+      backgroundColor: Color(0xff61bd86),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(caminhoimagem),
+            GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Image.asset("images/botao_voltar.png"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
